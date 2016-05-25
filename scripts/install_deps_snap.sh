ab25b6dc8ed8f881d2e2e6bca6886e92e655daad
krish <naveneetha@gmail.com>
2016-05-25 13:10:58 +0100

    fix parallel package name

--- scripts/install_deps_snap.sh
+++ scripts/install_deps_snap.sh
@@ -2,5 +2,5 @@
 
 yum install -y epel-release
 yum install -y gcc flex wget make patch
-yum install -y parallel
+yum install -y moreutils-parallel
 yum install -y java-1.8.0-openjdk java-1.8.0-openjdk-devel
